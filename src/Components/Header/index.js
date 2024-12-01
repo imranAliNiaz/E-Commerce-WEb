74d36b522238318c7ccf34ce7a0741db40616041
imranAliNiaz <121170880+imranAliNiaz@users.noreply.github.com>
2024-12-01 10:13:13 +0500

    First Commit

---
+++ src/Components/Header/index.js
@@ -0,0 +1,54 @@
+import { Link } from "react-router-dom";
+import Logo from "../../assets/images/logo.png";
+import CountryDropdown from "../CountryDropdown";
+import { Button } from "@mui/material";
+import { FiUser } from "react-icons/fi";
+import { IoBagOutline } from "react-icons/io5";
+import SearchBox from "./SearchBox";
+import Navigation from "./Navigation";
+const Header = () => {
+    return (
+        <>
+            <div className="headerWrapper">
+                <div className="top-strip bg-blue">
+                    <div className="container">
+                        <p className="mb-0 mt-0 text-center">Your Favorites are Just a <b>Click Away</b> – Start Shopping Now!</p>
+                    </div>
+                </div>
+
+                <header className="header">
+                    <div className="container">
+                        <div className="row">
+                            <div className="logoWrapper d-flex align-item-center col-sm-1">
+                                <Link to={'/'}><img src={Logo} alt="Logo"/></Link>
+                            </div>
+
+                            <div className="col-sm-10 d-flex align-items-center part2">
+                                <CountryDropdown/>
+                                <SearchBox/>
+
+                              <div className="part3 d-flex align-items-center ml-auto">
+                                <Button className="circle mr-3"><FiUser /></Button>
+                                <div className="ml-auto cartTab d-flex align-items-center">
+                                    <span className="price">$3.29</span>
+                                    <div className="position-relative ml-2">
+                                    <Button className="circle"><IoBagOutline /></Button>
+                                    <span className="count d-flex align-items-center justify-content-center">1</span>
+                                    </div>
+                                </div>
+                              </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </header>
+                
+                <Navigation/>
+
+               
+            </div>
+        </>
+    )
+}
+
+export default Header;
